d4cc85fd02386ad2ae036e0855466d0b8e82f262
张鹏举 <zpj2689@163.com>
2025-03-15 21:35:55 +0800

    修复引用错误

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 
 import zhCN from 'ant-design-vue/es/locale/zh_CN';
 </script>
